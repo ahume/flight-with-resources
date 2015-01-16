fd00aa0f451e4727d784de7afe193f0abc814562
Andy Hume <ahume@twitter.com>
2015-01-16 15:10:12 +0000

    Initial commit out of TweetDeck codebase.

---
+++ README.md
@@ -0,0 +1,80 @@
+# flight-with-resources
+
+[![Build Status](https://secure.travis-ci.org/<username>/flight-with-resources.png)](http://travis-ci.org/<username>/flight-with-resources)
+
+A [Flight](https://github.com/flightjs/flight) mixin for sharing named resources between components. A component can provide a resource into a central registry which other components can subsequently request. On component teardown any provided resources are removed.
+
+## Installation
+
+```bash
+bower install --save flight-with-resources
+```
+
+## Example
+
+Here's an example of two component definitions that use `withResources`.
+
+```js
+function providingComponent() {
+    this.after('initialize', function () {
+        this.provideResource('appManifest', {
+            version: '1.0.1',
+            buildNo: '1234',
+            url: 'https://tweetdeck.twitter.com'
+        });
+    });
+}
+
+function requestingComponent() {
+    this.after('initialize', function () {
+        var versionNo = this.requestResource('appManifest').versionNo;
+    });
+}
+```
+
+
+## API
+
+### `provideResource`
+
+`provideResources`, takes the name of the resource that is being provided, and then the resource itself. The resource can be any JavaScript type, including a function.
+
+### `requestResource`
+
+`requestResource` takes the name of the resource being requested and returns the requested resource.
+
+### `removeResource`
+
+`removeResource` takes the name of a resource to be removed.
+
+
+## Development
+
+Development of this component requires [Bower](http://bower.io) to be globally
+installed:
+
+```bash
+npm install -g bower
+```
+
+Then install the Node.js and client-side dependencies by running the following
+commands in the repo's root directory.
+
+```bash
+npm install & bower install
+```
+
+To continuously run the tests in Chrome during development, just run:
+
+```bash
+npm run watch-test
+```
+
+## Contributing to this project
+
+Anyone and everyone is welcome to contribute. Please take a moment to
+review the [guidelines for contributing](CONTRIBUTING.md).
+
+* [Bug reports](CONTRIBUTING.md#bugs)
+* [Feature requests](CONTRIBUTING.md#features)
+* [Pull requests](CONTRIBUTING.md#pull-requests)
---
+++ test/spec/with-resources.spec.js
@@ -0,0 +1,172 @@
+define(function (require) {
+    var defineComponent = require('flight/lib/component');
+    var withResources = require('lib/with-resources');
+    var Base = defineComponent(withResources);
+
+    describe('util/with_resources', function () {
+        // These tests just check that providing doesn't throw — we're testing the interface,
+        // and there isn't an interface beyond this.provideResource.
+        describe('provideResource', function () {
+            it('should be able to provide a named resource', function () {
+                var Component = Base.mixin(function () {
+                    this.after('initialize', function () {
+                        this.provideResource('cake', 'a nice cake!');
+                    });
+                });
+                expect(function () {
+                    (new Component).initialize(document.createElement('div'));
+                }).not.toThrow();
+            });
+
+            it('should be able to provide a named resource via a function', function () {
+                var Component = Base.mixin(function () {
+                    this.after('initialize', function () {
+                        this.provideResource('cake-fn', function () {
+                            return 'a nice cake from a function!';
+                        });
+                    });
+                });
+                expect(function () {
+                    (new Component).initialize(document.createElement('div'));
+                }).not.toThrow();
+            });
+
+            it('should not be able to provide the same named resource twice', function () {
+                var Component = Base.mixin(function () {
+                    this.after('initialize', function () {
+                        this.provideResource('cake-nope', 'nope');
+                    });
+                });
+                (new Component).initialize(document.createElement('div'));
+                expect(function () {
+                    (new Component).initialize(document.createElement('div'));
+                }).toThrow();
+            });
+        });
+
+        describe('requestResource', function () {
+            it('should be able to request a named resource', function () {
+                var Provider = Base.mixin(function () {
+                    this.after('initialize', function () {
+                        this.provideResource('cake-request', 'a nice cake!');
+                    });
+                });
+
+                var Requester = Base.mixin(function () {
+                    this.after('initialize', function () {
+                        expect(this.requestResource('cake-request')).toBe('a nice cake!');
+                    });
+                });
+
+                (new Provider).initialize(document.createElement('div'));
+                (new Requester).initialize(document.createElement('div'));
+            });
+
+            it('should be able to request a named resource from a function', function () {
+                var Provider = Base.mixin(function () {
+                    this.after('initialize', function () {
+                        this.provideResource('cake-fn-request', function () {
+                            return 'a nice cake from a function!';
+                        });
+                    });
+                });
+
+                var Requester = Base.mixin(function () {
+                    this.after('initialize', function () {
+                        expect(this.requestResource('cake-fn-request'))
+                            .toBe('a nice cake from a function!');
+                    });
+                });
+
+                (new Provider).initialize(document.createElement('div'));
+                (new Requester).initialize(document.createElement('div'));
+            });
+
+            it('should be able to request a named resource with some config', function () {
+                var Provider = Base.mixin(function () {
+                    this.after('initialize', function () {
+                        this.provideResource('cake-fn-config', function (config) {
+                            return config.str;
+                        });
+                    });
+                });
+
+                var Requester = Base.mixin(function () {
+                    this.after('initialize', function () {
+                        expect(this.requestResource('cake-fn-config', { str: 'cake from config!' }))
+                            .toBe('cake from config!');
+                    });
+                });
+
+                (new Provider).initialize(document.createElement('div'));
+                (new Requester).initialize(document.createElement('div'));
+            });
+
+            it('should call resource with correct context', function () {
+                var Provider = Base.mixin(function () {
+                    this.theCake = 'tasty cake!';
+                    this.after('initialize', function () {
+                        this.provideResource('cake-fn-tasty', function () {
+                            return this.theCake;
+                        });
+                    });
+                });
+
+                var Requester = Base.mixin(function () {
+                    this.after('initialize', function () {
+                        expect(this.requestResource('cake-fn-tasty'))
+                            .toBe('tasty cake!');
+                    });
+                });
+
+                (new Provider).initialize(document.createElement('div'));
+                (new Requester).initialize(document.createElement('div'));
+            });
+
+            it('should be able to delete a resource', function () {
+                var Provider = Base.mixin(function () {
+                    this.theCake = 'tasty cake!';
+                    this.after('initialize', function () {
+                        this.provideResource('cake-fn-remove', function () {
+                            return this.theCake;
+                        });
+                        this.removeResource('cake-fn-remove');
+                    });
+                });
+
+                var Requester = Base.mixin(function () {
+                    this.after('initialize', function () {
+                        this.requestResource('cake-fn-remove');
+                    });
+                });
+
+                (new Provider).initialize(document.createElement('div'));
+                expect(function () {
+                    (new Requester).initialize(document.createElement('div'));
+                }).toThrow();
+            });
+
+            it('should remove resources owned by component on teardown', function () {
+                var Provider = Base.mixin(function () {
+                    this.after('initialize', function () {
+                        this.provideResource('cake-owned', 'go on then');
+                    });
+                });
+                var Requester = Base.mixin(function () {
+                    this.after('initialize', function () {
+                        expect(this.requestResource('cake-owned'))
+                            .toBe('go on then');
+                    });
+                });
+
+                var p = (new Provider).initialize(document.createElement('div'));
+                (new Requester).initialize(document.createElement('div'));
+                // Teardown the providing component and check the next request throws.
+                p.teardown();
+                expect(function () {
+                    (new Requester).initialize(document.createElement('div'));
+                }).toThrow();
+            })
+        });
+    });
+});
